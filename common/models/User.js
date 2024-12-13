5edb7babd1247f734a0a690a8d0bea7032117e86
adityaraj <adityarajpatil25@gmail.com>
2024-12-13 23:12:37 +0530

    Defines schema in user.js

--- common/models/User.js
+++ common/models/User.js
@@ -1,1 +1,14 @@
-const  {DataTypes} = require(sequelize);
+const { DataTypes } = require("sequelize");
+const { roles } = require("../../config");
+const UserModel = {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+};
